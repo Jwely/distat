34f79f951976beb1f4fe63bbb26c2bf784d55d5f
Jeff Ely <jeff.ely.08@gmail.com>
2018-05-11 13:46:00 -0400

    revamped syntax scheme.

--- dice.py
+++ dice.py
@@ -1,71 +1,130 @@
-__author__ = 'Jwely'
-
-import numpy
+from typing import List, Union, Callable
+import numpy as np
 import matplotlib.pyplot as plt
 
-class D(object):
-    """
-    Simple dice class that allows more natural syntax for rolling dice
 
-    for example
-        4*D(6) will return a list of 4 dice objects
+__author__ = 'Jwely'
 
-    :param sides:   number of sides on die
-    :param dim      size of the array, for when parallel sets of rolls are desired
-    """
 
-    def __init__(self, sides, dim=None):
+class Modifier(object):
+
+    def __init__(self, mod: int = None):
+        if mod is None:
+            mod = 0
+        self.mod = mod
+
+    def __call__(self, other, *args):
+        def modified(other, *args):
+            return other(*args) + self.mod
+        return modified
+
+
+class Die(object):
+
+    def __init__(self, sides: int):
         self.sides = sides
-        if dim is not None:
-            self.dim = int(dim)
-        else:
-            self.dim = 1
 
-    def __rmul__(self, other):
+    def __call__(self, n:  Union[int, float] = None):
+        """ number of dice rolled decided when called """
+        if n is None:
+            n = 1
+
+        result = np.random.randint(1, self.sides + 1, int(n))
+
+        if n == 1:
+            result.reshape(1, 1)
+
+        return result
+
+    def __rmul__(self, other: int):
         """ defines leading integer multiplicative behavior of die """
+        assert isinstance(other, int)
+        return Pool([self for _ in range(other)])
 
-        if isinstance(other, int):
-            return [D(self.sides, self.dim) for i in range(other)]
-        elif isinstance(other, float):
-            return [D(self.sides, self.dim) for i in range(int(other))]
+
+class D(Die):
+    """ shorthand class for a Die"""
+    pass
+
+
+class Pool(object):
+
+    def __init__(self, dice: List[Die]):
+        """ A simple collection of multiple dice """
+        self.dice = dice
+
+    def __call__(self, *args):
+        rolls = [d(*args) for d in self.dice]
+        return np.column_stack(rolls)
+
+    def __add__(self, other):
+        """ modifies contents of dice pool """
+        if isinstance(other, Pool):
+            return Pool(self.dice + other.dice)
+        elif isinstance(other, Die):
+            return Pool(self.dice + other)
+
+
+class Highest(object):
+
+    def __init__(self, n: int):
+
+        self.n = n
+
+    def __call__(self, pool: Pool, *args):
+        rolls = np.sort(pool(*args))
+        return rolls[:, -self.n]
+
+
+class Lowest(object):
+    def __init__(self, n: int):
+        self.n = n
+
+    def __call__(self, pool: Pool, *args):
+        rolls = np.sort(pool(*args))
+        return rolls[:, self.n]
+
+
+class Add(object):
+    def __call__(self, pool: Pool, *args):
+        rolls = np.sum(pool(*args))
+        return rolls
+
+Aggregator = Union[Highest, Lowest, Add]
+
+
+class Check(object):
+
+    def __init__(self, agg: Aggregator, min: int = None, max: int = None):
+        self.agg = agg
+        self.min = min
+        self.max = max
+
+    def __call__(self, pool: Pool, *args):
+        rolls = self.agg(pool, *args)
+        if self.min is not None and self.max is not None:
+            return self.min < rolls < self.max
+        elif self.min is not None:
+            return self.min < rolls
+        elif self.max is not None:
+            return self.max > rolls
         else:
-            raise Exception("Can only have integer numbers of die")
-
-    def roll(self):
-        return numpy.random.randint(1, self.sides + 1, self.dim)
+            return rolls
 
 
-def highest(num, rolls):
-    """ returns the highest "num" of dice from rolls matrix """
-    ordered = numpy.sort(rolls)
-    return ordered[:, -num:]
+class Roll(object):
 
+    def __init__(self, pool: Pool, agg: Aggregator = None):
+        """ combines a pool and an aggregator to produce a rolled result """
+        # dummy function for aggregator
+        if agg is None:
+            agg = Add()
 
-def lowest(num, rolls):
-    """ returns the lowest 'num' of dice from rolls matrix """
-    ordered = numpy.sort(rolls)
-    return ordered[:, :num]
+        self.pool = pool
+        self.agg = agg
 
-
-def roll(dice):
-    """
-    :param dice:    list containing D objects and/or constants
-    :return:        A roll matrix, depending on the dim of input D
-    """
-
-    dim = [item.dim for item in dice if isinstance(item, D)]
-    if len(list(set(dim))) > 1:
-        raise Exception("dice must have same dimensions")
-
-    out = numpy.ndarray((int(dim[0]), len(dice)), dtype="int")
-
-    for i, item in enumerate(dice):
-        if isinstance(item, D):
-            out[:, i] = item.roll()
-        elif isinstance(item, int):
-            out[:, i] = numpy.ndarray(len(dice)) + item
-
-    return out
+    def __call__(self, *args):
+        return self.agg(self.pool, *args)
 
 
 def histogram(rolls, title=None, xmax=None, ymax=None):
@@ -77,15 +136,15 @@ def histogram(rolls, title=None, xmax=None, ymax=None):
     """
 
     if len(rolls.shape) != 1:
-        rolls = numpy.sum(rolls, axis=1)
+        rolls = np.sum(rolls, axis=1)
 
     plt.hist(rolls, bins=range(min(rolls), max(rolls) + 2),
              align="left", edgecolor='k', facecolor="darksalmon",
-             normed=True, rwidth=0.9)
-    plt.axvline(numpy.mean(rolls), color="navy", linestyle="--", linewidth=2)
-    plt.axvline(numpy.median(rolls), color="black", linestyle=":", linewidth=2)
-    plt.legend(["mean = {:0.2f}".format(numpy.mean(rolls)),
-                "median = {:0.0f}".format(numpy.median(rolls)),
+             density=True, rwidth=0.9)
+    plt.axvline(np.mean(rolls), color="navy", linestyle="--", linewidth=2)
+    plt.axvline(np.median(rolls), color="black", linestyle=":", linewidth=2)
+    plt.legend(["mean = {:0.2f}".format(np.mean(rolls)),
+                "median = {:0.0f}".format(np.median(rolls)),
                 "histogram"])
     plt.ylabel("Rate of occurrence")
     plt.xlabel("Value")
@@ -108,23 +167,24 @@ def histogram(rolls, title=None, xmax=None, ymax=None):
 # now we can very quickly do monte carlo simulations on various dice rolls
 if __name__ == "__main__":
 
-    dims = 1e7
-
     # syntax examples
-    rolls = roll(3 * D(6, dims))
-    histogram(rolls, "3_summed_3D6", ymax=0.2)
+    roll = Roll(2 * D(20), Highest(1))
+    histogram(roll(1e6))
 
-    rolls = highest(3, roll(4 * D(6, dims)))
-    histogram(rolls, '3_highest_4D6', ymax=0.2)
-
-    rolls = highest(1, roll(2 * D(20, dims)))
-    histogram(rolls, "highest_of_2_d20", ymax=0.2)
-
-    rolls = lowest(1, roll(2 * D(20, dims)))
-    histogram(rolls, "lowest_of_2_d20", ymax=0.2)
-
-    rolls = highest(3, roll(5 * D(6, dims)))
-    histogram(rolls, "3_highest_5D6", ymax=0.2)
-
-    rolls = highest(3, roll(6 * D(6, dims)))
-    histogram(rolls, "3_highest_6D6", ymax=0.2)
+    # rolls = roll(3 * D(6, dims))
+    # histogram(rolls, "3_summed_3D6", ymax=0.2)
+    #
+    # rolls = highest(3, roll(4 * D(6, dims)))
+    # histogram(rolls, '3_highest_4D6', ymax=0.2)
+    #
+    # rolls = highest(1, roll(2 * D(20, dims)))
+    # histogram(rolls, "highest_of_2_d20", ymax=0.2)
+    #
+    # rolls = lowest(1, roll(2 * D(20, dims)))
+    # histogram(rolls, "lowest_of_2_d20", ymax=0.2)
+    #
+    # rolls = highest(3, roll(5 * D(6, dims)))
+    # histogram(rolls, "3_highest_5D6", ymax=0.2)
+    #
+    # rolls = highest(3, roll(6 * D(6, dims)))
+    # histogram(rolls, "3_highest_6D6", ymax=0.2)
