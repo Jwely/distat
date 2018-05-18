73c4ae95717b726e3f2e69d2b7ceadd17f3c7e33
Jeff Ely <jeff.ely.08@gmail.com>
2018-05-17 22:53:13 -0400

    something

--- dice.py
+++ dice.py
@@ -1,5 +1,8 @@
 from typing import List, Union, Callable
 import numpy as np
+
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
@@ -13,16 +16,18 @@ class Modifier(object):
             mod = 0
         self.mod = mod
 
-    def __call__(self, other, *args):
-        def modified(other, *args):
-            return other(*args) + self.mod
-        return modified
+    def __call__(self, arr: np.array):
+        return arr + self.mod
 
 
 class Die(object):
 
     def __init__(self, sides: int):
         self.sides = sides
+
+    def __repr__(self):
+        """representation of the die """
+        return f"D{int(self.sides)}"
 
     def __call__(self, n:  Union[int, float] = None):
         """ number of dice rolled decided when called """
@@ -40,6 +45,12 @@ class Die(object):
         """ defines leading integer multiplicative behavior of die """
         assert isinstance(other, int)
         return Pool([self for _ in range(other)])
+
+    def __add__(self, other):
+        if isinstance(other, Die):
+            return Pool([self, other])
+        elif isinstance(other, Pool):
+            return Pool + other
 
 
 class D(Die):
@@ -62,35 +73,154 @@ class Pool(object):
         if isinstance(other, Pool):
             return Pool(self.dice + other.dice)
         elif isinstance(other, Die):
-            return Pool(self.dice + other)
+            return Pool(self.dice + [other])
+
+    def __repr__(self):
+        """representation of the pool"""
+        return "[{}]".format(', '.join([d.__repr__() for d in self.dice]))
 
 
-class Highest(object):
+class _Op(object):
+    """ workaround for typehinting quirk? """
 
-    def __init__(self, n: int):
+    def __call__(self, arr: np.array):
+        return arr
+
+
+class _Aggregator(object):
+    """workaround for typhinting quirk?"""
+
+    def __call__(self, arr: np.array):
+        return arr
+
+
+class Op(_Op):
+
+    def __sub__(self, other):
+        def sub(arr: np.array):
+            print(self.__class__.__name__, "sub")
+            return self(arr) - other(arr)
+        return sub
+
+    def __neg__(self):
+        def neg(arr: np.array):
+            print(self.__class__.__name__, "neg")
+            return - self(arr)
+
+        return neg
+
+    def __add__(self, other: Union[int, np.array, _Op, _Aggregator]):
+        def add(_other):
+            print(self.__class__.__name__, "add")
+            if isinstance(_other, _Op) or isinstance(_other, _Aggregator):
+                return _other()
+            else:
+                return np.sum(_other, axis=1)
+        return add
+
+    def __pos__(self, other):
+        print(self.__class__.__name__, "pos")
+        return self.__add__(other)
+
+
+class Aggregator(_Aggregator):
+
+    def __init__(self,
+                 ops: Union[
+                     List[Callable[[np.array], np.array]],
+                     Op]
+                 ):
+        if isinstance(ops, list):
+            self.ops = ops
+        elif isinstance(ops, Aggregator):
+            self.ops = ops.ops
+        elif isinstance(ops, Op):
+            self.ops = [ops]
+
+    def __call__(self, arr: np.array):
+        """ chains the list of operations """
+
+        result = arr
+        for op in self.ops:
+            result = op(result)
+
+        return result
+
+    def __sub__(self, other):
+        def sub(arr: np.array):
+            print(self.__class__.__name__, "sub")
+            return self(arr) - other(arr)
+        return sub
+
+    def __add__(self, other: Union[int, np.array, _Op, _Aggregator]):
+        def add(_other):
+            print(self.__class__.__name__, "add")
+            if isinstance(_other, _Op) or isinstance(_other, _Aggregator):
+                return _other()
+            else:
+                return np.sum(_other, axis=1)
+        return add
+
+
+class _Sum(object):
+
+    def __call__(self, arr: np.array):
+        print(self.__class__.__name__, arr.shape)
+        if len(arr.shape) > 1:
+            return np.sum(arr, axis=1)
+        else:
+            return arr
+
+
+class Sum(Aggregator):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ops += [_Sum()]
+        print(self.ops)
+
+
+class Highest(Op):
+
+    def __init__(self, n: int = None):
 
         self.n = n
 
-    def __call__(self, pool: Pool, *args):
-        rolls = np.sort(pool(*args))
-        return rolls[:, -self.n]
+    def __call__(self, arr: np.array):
+        print(self.__class__.__name__, arr.shape)
+        rolls = np.sort(arr)
+        return rolls[:, -self.n:]
 
 
-class Lowest(object):
-    def __init__(self, n: int):
+class Lowest(Op):
+    def __init__(self, n: int = None):
         self.n = n
 
-    def __call__(self, pool: Pool, *args):
-        rolls = np.sort(pool(*args))
-        return rolls[:, self.n]
+    def __call__(self, arr: np.array):
+        print(self.__class__.__name__, arr.shape)
+        rolls = np.sort(arr)
+        return rolls[:, :self.n]
 
 
-class Add(object):
-    def __call__(self, pool: Pool, *args):
-        rolls = np.sum(pool(*args))
-        return rolls
+class Pos(Op):
+    def __init__(self, *args):
 
-Aggregator = Union[Highest, Lowest, Add]
+        self._ndims = len(args)
+
+        if self._ndims > 1:
+            self.slice = slice(*args)
+        else:
+            self.slice = args[0]
+
+        print(self.slice)
+
+    def __call__(self, arr: np.array):
+        print(self.__class__.__name__)
+        result = arr[:, self.slice]
+        if self._ndims == 1:
+            return result.reshape(-1,1)
+        else:
+            return result
 
 
 class Check(object):
@@ -114,17 +244,29 @@ class Check(object):
 
 class Roll(object):
 
-    def __init__(self, pool: Pool, agg: Aggregator = None):
+    def __init__(self, pool: Pool, agg: Aggregator = None, mod: Modifier = None):
         """ combines a pool and an aggregator to produce a rolled result """
         # dummy function for aggregator
         if agg is None:
-            agg = Add()
+            agg = Op()
+
+        if mod is None:
+            mod = Modifier(0)
 
         self.pool = pool
         self.agg = agg
+        self.mod = mod
+
+        print(self.pool)
 
     def __call__(self, *args):
-        return self.agg(self.pool, *args)
+        p = self.pool(*args)
+        b = self.agg(p)
+        c = self.mod(b)
+        print(p)
+        print(b)
+        print(c)
+        return c
 
 
 def histogram(rolls, title=None, xmax=None, ymax=None):
@@ -166,29 +308,15 @@ def histogram(rolls, title=None, xmax=None, ymax=None):
 
 # now we can very quickly do monte carlo simulations on various dice rolls
 if __name__ == "__main__":
-
+    pass
     # syntax examples
-    # roll = Roll(2 * D(20), Highest(1))
+    # roll = Roll(2 * D(20), Lowest(1), Modifier(0))
     # histogram(roll(1e6))
 
-    # this doesnt work! it should!
-    roll = Roll(4 * D(6), Add(Highest(3)))
-    histogram(roll(1e6))
+    #roll = Roll(4 * D(6), Highest(3))
+    #roll = Roll(3 * D(6))
+    #histogram(roll(1e6))
 
-    # rolls = roll(3 * D(6, dims))
-    # histogram(rolls, "3_summed_3D6", ymax=0.2)
-    #
-    # rolls = highest(3, roll(4 * D(6, dims)))
-    # histogram(rolls, '3_highest_4D6', ymax=0.2)
-    #
-    # rolls = highest(1, roll(2 * D(20, dims)))
-    # histogram(rolls, "highest_of_2_d20", ymax=0.2)
-    #
-    # rolls = lowest(1, roll(2 * D(20, dims)))
-    # histogram(rolls, "lowest_of_2_d20", ymax=0.2)
-    #
-    # rolls = highest(3, roll(5 * D(6, dims)))
-    # histogram(rolls, "3_highest_5D6", ymax=0.2)
-    #
-    # rolls = highest(3, roll(6 * D(6, dims)))
-    # histogram(rolls, "3_highest_6D6", ymax=0.2)
+    #roll = Roll(4 * D(6), Sum(Highest(2)) - Sum(Lowest(2)))
+    #r = roll(1e6)
+    #histogram(r)
